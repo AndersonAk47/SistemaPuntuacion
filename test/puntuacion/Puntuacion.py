d35e21630f2d757b4fb5a12dd12e93377d25651f
AndersonAk47 <Anderson99ro@gmail.com>
2025-02-28 00:11:18 -0500

    third commit

--- test/puntuacion/Puntuacion.py
+++ test/puntuacion/Puntuacion.py
@@ -23,15 +23,3 @@ def test_puntuacion_igual_o_mayor_a_50_sin_bonus():
    result : str = calcular_puntuacion(puntos , bonus) # Llamamos a la funcion y guardamos el resultdo
    assert expect == result  # Comprobamos que el resulta sea el esperado
   
-
-
-def test_puntuacion_igual_o_mayor_a_50_sin_bonus():
-   """"
-   Definimos la funcion para verificar si obtiene el Plata 
-
-   """
-   puntos : int=  63       # Damos los puntos
-   bonus : bool = ""      # Damos los bonus
-   expect: str  = "Plata"     # Resultado esperado 
-   result : str = calcular_puntuacion(puntos , bonus) # Llamamos a la funcion y guardamos el resultdo
-   assert expect == result  # Comprobamos que el resulta sea el esperado
